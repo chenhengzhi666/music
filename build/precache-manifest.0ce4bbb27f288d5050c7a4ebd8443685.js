734ac5b33e0149e83b5de4dd1e91ce2340cd4155
chenhengzhi <hengzhi.chen@foxmail.com>
2019-12-06 13:03:23 +0800

    测试

---
+++ build/precache-manifest.0ce4bbb27f288d5050c7a4ebd8443685.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0f7eb76ffe9150b6e88a64a27d116c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c133ef5d49a3c716fa2",
+    "url": "./static/css/2.a2a143f0.chunk.css"
+  },
+  {
+    "revision": "111788f9433487f09da4",
+    "url": "./static/css/main.58a868d6.chunk.css"
+  },
+  {
+    "revision": "8c133ef5d49a3c716fa2",
+    "url": "./static/js/2.c6e31732.chunk.js"
+  },
+  {
+    "revision": "111788f9433487f09da4",
+    "url": "./static/js/main.b524a4c4.chunk.js"
+  },
+  {
+    "revision": "8fd55d693771d80abb13",
+    "url": "./static/js/runtime-main.b4c9b231.js"
+  },
+  {
+    "revision": "473725c8cff7cd5cde67592ffccd2a55",
+    "url": "./static/media/default_play_bg.473725c8.jpg"
+  },
+  {
+    "revision": "500114c437d5c8725fcddeea01fda7ec",
+    "url": "./static/media/iconfont.500114c4.woff"
+  },
+  {
+    "revision": "5012fcac912bee3591516c32bd4a3f56",
+    "url": "./static/media/iconfont.5012fcac.svg"
+  },
+  {
+    "revision": "a130fe7c29e11202d4ab108c41aa8c38",
+    "url": "./static/media/iconfont.a130fe7c.eot"
+  },
+  {
+    "revision": "f41f5790dfbfa2a7d568fde2e36b1364",
+    "url": "./static/media/iconfont.f41f5790.ttf"
+  }
+]);
--- config-overrides.js
+++ config-overrides.js
@@ -5,6 +5,14 @@ const {
     addWebpackAlias
 } = require('customize-cra');
 
+// const path = require('path');
+// const paths = require('react-scripts/config/paths');
+// 修改打包文件路径（build => dist）
+// paths.appBuild = path.join(path.dirname(paths.appBuild), 'dist');
+
+// 关闭 sourcemap
+process.env.GENERATE_SOURCEMAP = "false";
+
 module.exports = {
     webpack: override(
         addLessLoader(),
@@ -21,6 +29,11 @@ module.exports = {
                         selectorBlackList: ['am-']
                     })
             ]
-        ),
-    )
+        )
+    ),
+    paths: (paths, env) => {
+        // console.log(paths)
+        // ...add your paths config
+        return paths;
+    },
 }
