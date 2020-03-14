53817353043922401d3fcee8d9787299bcb03d05
hengzhi.chen <hengzhi.chen@foxmail.com>
2020-03-14 12:03:15 +0800

    2020/3/14/周六

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+// 测试 sourceTree
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
