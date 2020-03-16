39a5c41ff873432c1e2b98b0c4be240cd693d3b8
hengzhi.chen <hengzhi.chen@foxmail.com>
2020-03-16 10:35:32 +0800

    111

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Header from '@/components/header';
-import '@/assets/less/index.less'
-import RouterMap from '@/router/index.js'
-import { Provider }  from 'react-redux'
-import store from '@/redux/store'
-import { HashRouter as Router } from 'react-router-dom'
-import MusicPlayer from '@/components/player'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Header from "@/components/header";
+import "@/assets/less/index.less";
+import RouterMap from "@/router/index.js";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
+import { HashRouter as Router } from "react-router-dom";
+import MusicPlayer from "@/components/player";
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <Header />
-            <RouterMap />
-            <MusicPlayer />
-        </Router>
-    </Provider>, 
-    document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <Header />
+      <RouterMap />
+      <MusicPlayer />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // 测试 sourceTree
